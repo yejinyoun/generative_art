36a23e9cd1b5dfea69dbb00ec0c03b2925767f39
yejinyoun <yejinyoun@icloud.com>
2023-02-09 17:06:07 +0100

    artwork2

--- art.js
+++ art.js
@@ -8,6 +8,7 @@ console.log("art!!!!");
 //
 
 art1();
+art2();
 
 function art1() {
   for (let i = 100; i <= 300; i += 20) {
@@ -16,6 +17,19 @@ function art1() {
     box.style.width = `${i}px`;
     box.style.height = `${i}px`;
     document.querySelector("#artwork1").appendChild(box);
+
+    console.log(i);
+  }
+}
+
+// 2. generates a div that has class of .box, and rotate;
+
+function art2() {
+  for (let i = 0; i <= 300; i += 10) {
+    const box = document.createElement("div");
+    box.className = "box";
+    document.querySelector("#artwork2").appendChild(box);
+    box.style.transform = `rotate(${i}deg)`;
 
     console.log(i);
   }
